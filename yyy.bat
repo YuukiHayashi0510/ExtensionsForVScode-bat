a52e6e4eaa27dad7c37cf9f06c8d2bdc93f111a8
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-08-02 17:32:35 +0900

    テストファイル削除削除

--- yyy.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo start to install VSCode extentions.
-code --install-extension wingrunr21.vscode-ruby & code --install-extension xabikos.JavaScriptSnippets & code --install-extension Zignd.html-css-class-completion & echo finish installing... & pause
-exit
